3ea36c52d5bd8dc6bdde02b04d49a4839c20a707
Chrismaganga <chrismaganga3@gmail.com>
2023-07-21 13:35:55 +0200

    add all

---
+++ app/api/route.ts
@@ -0,0 +1,36 @@
+import { OpenAIStream, StreamingTextResponse } from "ai";
+import { Configuration, OpenAIApi } from "openai-edge";
+import { prisma } from "../db";
+
+
+const config = new Configuration({
+  apiKey: process.env.OPENAI_API_KEY,
+});
+
+const openai = new OpenAIApi(config);
+
+export const runtime = "edge";
+
+export async function POST(req: Request) {
+  const { messages } = await req.json();
+
+  const response = await openai.createChatCompletion({
+    model: "gpt-3.5-turbo",
+    stream: true,
+    messages: messages,
+  });
+
+  const stream = OpenAIStream(response, {
+    onCompletion:async (completion:string) => {
+      const  data = await prisma.message.create({
+        data: {
+          answer: completion,
+          question: messages,
+        }
+      })
+      
+    }
+  });
+
+  return new StreamingTextResponse(stream);
+}
